622b70f1710e6d31d0d48c05876fe6906b761b77
edralzar <edralzar@gmail.com>
2013-01-06 23:10:56 +0100

    added method to retrieve an Article

--- src/main/java/net/edralzar/jreadability/service/ReadabilityService.java
+++ src/main/java/net/edralzar/jreadability/service/ReadabilityService.java
@@ -1,9 +1,16 @@
 package net.edralzar.jreadability.service;
 
+import net.edralzar.jreadability.data.Article;
+import net.edralzar.jreadability.oauth.ReadabilityConst;
 import net.edralzar.jreadability.oauth.store.ITokenStore;
 
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
 import org.scribe.model.Token;
+import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
+
+import com.google.gson.Gson;
 
 public class ReadabilityService {
 
@@ -17,6 +24,18 @@ public class ReadabilityService {
 
 	public BookmarkListFinder getBookmarks() {
 		return new BookmarkListFinder(this.token, this.service);
+	}
+
+	public Article getArticle(String id) {
+		OAuthRequest request = new OAuthRequest(Verb.GET,
+				ReadabilityConst.API_ARTICLE + id);
+
+		service.signRequest(token, request);
+		Response response = request.send();
+
+
+		Gson gson = ReadabilityServiceBuilder.newGson();
+		return gson.fromJson(response.getBody(), Article.class);
 	}
 
 }
