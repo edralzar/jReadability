24ed84ea417cdba3ec2916e091b1fc16bf9692b5
edralzar <edralzar@gmail.com>
2013-01-06 21:03:38 +0100

    added pattern for dateTime

--- src/main/java/net/edralzar/jreadability/oauth/ReadabilityConst.java
+++ src/main/java/net/edralzar/jreadability/oauth/ReadabilityConst.java
@@ -2,6 +2,8 @@ package net.edralzar.jreadability.oauth;
 
 public interface ReadabilityConst {
 
-	public static final String API_KEY = "edralzar1";
-	public static final String API_SECRET = "xf4mxqhnBJyWWpFFMznHsjSbk7SVsdKf";
+	static final String API_KEY = "edralzar1";
+	static final String API_SECRET = "xf4mxqhnBJyWWpFFMznHsjSbk7SVsdKf";
+
+	public static final String DATETIME_PATTERN = "yyyyMMdd'T'hh:mm:ss";
 }
