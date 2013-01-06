cac690f2a2bef42e05d905c55ea9a8b5cb84a8ec
edralzar <edralzar@gmail.com>
2013-01-06 15:03:55 +0100

    dependency to scribe and gson added readability api interface for scribe

---
+++ src/main/java/net/edralzar/jreadability/oauth/ReadabilityConst.java
@@ -0,0 +1,7 @@
+package net.edralzar.jreadability.oauth;
+
+public interface ReadabilityConst {
+
+	public static final String API_KEY = "edralzar1";
+	public static final String API_SECRET = "xf4mxqhnBJyWWpFFMznHsjSbk7SVsdKf";
+}
